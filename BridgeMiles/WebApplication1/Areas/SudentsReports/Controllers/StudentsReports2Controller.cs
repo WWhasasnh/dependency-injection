d9a3d74425ccb8dcad96ce309922fe59d2b21b77
MHasasneh <mhasasneh@arkesystems.com>
2018-08-12 11:56:17 +0300

    initial V

---
+++ BridgeMiles/WebApplication1/Areas/SudentsReports/Controllers/StudentsReports2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Areas.SudentsReports.Controllers
+{
+    public class StudentsReports2Controller : Controller
+    {
+        // GET: SudentsReports/StudentsReports
+        public ActionResult StudentsAttendance()
+        {
+            return View();
+        }
+    }
+}
